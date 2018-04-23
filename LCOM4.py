634c42e226f75c5ee2e1ef230061670c4daec652
pengzedong <36201209+pengzedong@users.noreply.github.com>
2018-04-23 00:25:46 -0400

    Update LCOM4.py

--- LCOM4.py
+++ LCOM4.py
@@ -1,8 +1,8 @@
 import ast
 
 # path = 'sample2.py'
-path = 'SampleIncludingAll1.py'
-tree = ast.parse(open(path).read())
+# path = 'SampleIncludingAll1.py'
+# tree = ast.parse(open(path).read())
 
 
 
@@ -37,7 +37,7 @@ class LOCM4():
                                         FunctionDef_list.append(visitor.attr_name)
                             ClassDef_list.append(FunctionDef_list)
                 main_list.append(ClassDef_list)
-
+        #print(main_list)
 
 
         def find_key(v, a):
@@ -48,24 +48,28 @@ class LOCM4():
 
 
         ClassDef_list = []
-        for node in ast.walk(tree):
+        for node in ast.walk(ast.parse(open(self.p_way).read())):
             if isinstance(node, ast.ClassDef):
                 ClassDef_list.append(node.name)
+
 
         for i in range(len(main_list)):
             h = main_list[i]
             templist = []
             templist2 = {}
             key = 1
-            for i in range(0, len(h) - 1):
-                for g in range(i + 1, len(h)):
-                    if (set(h[i]) & set(h[g])) != set():
-                        if any(i in val for val in templist2.values()):
-                            yy = find_key(templist2, i)
-                            templist2[yy].append(g)
-                        else:
-                            templist2[key] = [i, g]
-                            key = key + 1
-            # print(templist2)
-            result+=ClassDef_list[i]+ '= '+ str(len(templist2))+'\n'
+            count_cohesion=len(h)
+            for k in range(0, len(h)-1):
+                for g in range(k + 1, len(h)):
+                    if (set(h[k]) & set(h[g])) != set():
+                        count_cohesion-=1
+                        # if any(i in val for val in templist2.values()):
+                        #     yy = find_key(templist2, i)
+                        #     templist2[yy].append(g)
+                        # else:
+                        #     templist2[key] = [i, g]
+                        #     key = key + 1
+
+
+            result+=ClassDef_list[i]+ '= '+ str(count_cohesion)+'\n'
         return result
